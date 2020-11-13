ba7a518a176a2aa3ebd7860fcae9d11cd7d76113
Michal Nadstawny <michnadstawny@gmail.com>
2020-11-13 02:43:43 +0100

    .gitignore, dotnet project setup, tutorial start

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Michal", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
