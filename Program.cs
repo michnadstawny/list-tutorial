8649435796976242f9e338d6a33cf82b0dd162ad
Michal Nadstawny <michnadstawny@gmail.com>
2020-11-13 03:14:27 +0100

    challenge completed

--- Program.cs
+++ Program.cs
@@ -53,10 +53,13 @@ namespace list_tutorial
         {
             //WorkingWithStrings();
             var fibonacciNumbers = new List<int> { 1, 1 };
-            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+            while (fibonacciNumbers.Count < 20)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
 
-            fibonacciNumbers.Add(previous + previous2);
+                fibonacciNumbers.Add(previous + previous2);
+            }
 
             foreach (var item in fibonacciNumbers)
                 Console.WriteLine(item);
