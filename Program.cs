c1b4dac1f58017ddc01d1d8f62ecb8f9ab32fb71
Michal Nadstawny <michnadstawny@gmail.com>
2020-11-13 03:07:53 +0100

    fibonacciNumbers

--- Program.cs
+++ Program.cs
@@ -51,7 +51,15 @@ namespace list_tutorial
         }
         static void Main(string[] args)
         {
-            WorkingWithStrings();
+            //WorkingWithStrings();
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(previous + previous2);
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
         }
     }
 }
